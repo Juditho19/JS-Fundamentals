4f545e1396704a9261adad8a631e186fc952b166
Juditho19 <156449886+Juditho19@users.noreply.github.com>
2025-05-25 23:45:01 +0100

    7-multi_c.js added again

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const count = parseInt(process.argv[2]);
+
+if (isNaN(count)) {
+  console.log('Missing number of occurrences');
+} else if (count > 0) {
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
+}
+// If count <= 0, print nothing
