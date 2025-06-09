e6fb19930f1578d69948f33acddbe5f3f16d5f25
Juditho19 <156449886+Juditho19@users.noreply.github.com>
2025-06-09 12:59:56 +0100

    Update 3-value_argument.js
    added alternative

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -4,3 +4,13 @@ if (arg === undefined) {
 } else {
   console.log(arg);
 } 
+
+ 
+// or
+// const args = process.argv.slice(2)
+
+// if (args[0] === undefined){
+//     console.log("No argument");
+// } else {
+//     console.log(args[0]);
+// }
